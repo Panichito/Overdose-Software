15afca79d833386ec90e8ea06f266cb1c7484574
Miran-Mirantee <killerm11805@hotmail.com>
2022-10-27 23:06:38 +0700

    Styled appBar

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
   //const HomePage({super.key});
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -12,12 +11,61 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Home Page'),
-        backgroundColor: Colors.green[400],
+        titleSpacing: 24,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  'Welcome Back,',
+                  style: TextStyle(
+                    fontSize: 16,
+                  ),
+                ),
+                SizedBox(height: 10),
+                Text(
+                  'User',
+                  style: TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ],
+            ),
+            const CircleAvatar(
+              radius: 48,
+            ),
+          ],
+        ),
+        backgroundColor: Colors.cyanAccent[400],
+        toolbarHeight: 200,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(24),
+          ),
+        ),
       ),
-      body: Padding(
-        padding: EdgeInsets.all(15),
-        child: Text('Flutter ROCK not implement yed!!'),
+
+      body: Column(
+
+      ),
+      // body: const Padding(
+      //   padding: EdgeInsets.all(15),
+      //   child: Text(
+      //     'Flutter ROCK!!',
+      //     style: TextStyle(
+      //       fontSize: 30,
+      //       fontWeight: FontWeight.bold,
+      //     ),
+      //   ),
+      // ),
+      bottomNavigationBar: BottomAppBar(
+        child: Container(
+          height: 50,
+          color: Colors.cyanAccent[400],
+        ),
       ),
     );
   }
