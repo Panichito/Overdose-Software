817ab1cf3814abb2cc227c62f14955ad0ad20090
Miran-Mirantee <killerm11805@hotmail.com>
2022-11-03 15:43:31 +0700

    Create no suggestion search field

---
+++ app/lib/pages/noSuggestSearch.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:searchfield/searchfield.dart';
+
+// put dummy in search suggestion
+class Dummy {
+  String dummy;
+  Dummy(this.dummy);
+}
+
+// create dummy list
+List<Dummy> dummy = [
+  Dummy(''),
+];
+
+Widget noSuggestSearch(updateList(String value)) {
+  return SearchField(
+    suggestions: dummy.map((e) => SearchFieldListItem(
+      e.dummy,
+      item: e,
+    )).toList(),
+    searchStyle: const TextStyle(fontSize: 18,),
+    suggestionStyle: const TextStyle(fontSize: 18,),
+    searchInputDecoration: const InputDecoration(
+      border: OutlineInputBorder(),
+      contentPadding: EdgeInsets.fromLTRB(8, 16, 8, 16),
+      prefixIcon: Icon(Icons.search),
+    ),
+    maxSuggestionsInViewPort: 0,
+    onSubmit: (value) => updateList(value),
+  );
+}
