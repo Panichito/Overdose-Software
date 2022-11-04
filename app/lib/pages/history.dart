1e58f5b886586b602175c231aa045e194249be73
Panichito <panithi2001big@gmail.com>
2022-11-04 21:03:26 +0700

    history page appbar (with back button)

--- app/lib/pages/history.dart
+++ app/lib/pages/history.dart
@@ -34,6 +34,7 @@ class _HistoryPageState extends State<HistoryPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(title: Text('History by Madam Gus'), backgroundColor: Colors.indigo[400]),
       body: Container(
         child: (ListView.builder(
         itemBuilder: (context, index) {
