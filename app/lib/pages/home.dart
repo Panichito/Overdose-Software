d65d4c77e2bced0100141ad77e34112d8c1a3e12
Miran-Mirantee <killerm11805@hotmail.com>
2022-11-25 01:09:38 +0700

    Add temp schedules

--- app/lib/pages/home.dart
+++ app/lib/pages/home.dart
@@ -2,6 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:app/pages/noSuggestSearch.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
+// temp patient constructor
+class Schedule {
+  String id;
+  String recordId;
+  String time;
+  String medicine;
+  int amount;
+  bool isTaken = false;
+
+  Schedule(this.id, this.recordId, this.time, this.medicine, this.amount);
+}
+
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
   @override
@@ -11,6 +23,17 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String username="", profilepic="https://t4.ftcdn.net/jpg/03/49/49/79/360_F_349497933_Ly4im8BDmHLaLzgyKg2f2yZOvJjBtlw5.jpg";
 
+  List<Schedule> allSchedule = [
+    Schedule('1', '1', '12:34', 'Ya Ba', 69),
+    Schedule('2', '1', '12:34', 'Ya Ma', 13),
+    Schedule('3', '1', '12:34', 'Ya E', 11),
+    Schedule('4', '1', '12:34', 'Ya Tum yang nee', 1),
+    Schedule('5', '1', '12:34', 'Mai wa gub krai', 2),
+    Schedule('6', '1', '12:34', 'Kao jai mai?', 3),
+    Schedule('7', '1', '12:34', 'Bird Thongchai', 4),
+
+  ];
+
   @override
   void initState() {
     // TODO: implement initState
@@ -19,6 +42,57 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
+  Widget scheduleCard(Schedule schedule) {
+    return Card(
+      color: schedule.isTaken?
+        Colors.green[100]:
+        Colors.red[100],
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10),
+      ),
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(schedule.medicine, style: TextStyle(fontSize: 32.0, color: Colors.grey[800])),
+                Text('Amount: ${schedule.amount}', style: TextStyle(fontSize: 16.0, color: Colors.grey[800])),
+                const SizedBox(height: 20),
+                Text('Time: ${schedule.time}', style: TextStyle(fontSize: 20.0, color: Colors.grey[800])),
+                // const SizedBox(height: 4),
+                // Text('Schedule-id: S${schedule.id}', style: TextStyle(fontSize: 16.0, color: Colors.grey[800])),
+                // const SizedBox(height: 4),
+                // Text('Record-id: R${schedule.recordId}', style: TextStyle(fontSize: 16.0, color: Colors.grey[800])),
+              ],
+            ),
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      // set isTaken to false
+                      setState(() {
+                        schedule.isTaken = !schedule.isTaken;
+                      });
+                    },
+                    style: ElevatedButton.styleFrom(
+                      padding: EdgeInsets.all(16),
+                      backgroundColor: Colors.grey[700],
+                    ),
+                    child: !schedule.isTaken?
+                      const Text('Take', style: TextStyle(fontSize: 20),):
+                      const Text('Untake', style: TextStyle(fontSize: 20),),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
@@ -66,15 +140,11 @@ class _HomePageState extends State<HomePage> {
             child: noSuggestSearch((value) => null),
           ),
           Expanded(
-            flex: 1,
-            child: Container(
-              color: Colors.grey[200],
-              child: const Center(
-                // check if schedule exist
-                // ** condition not yet implemented **
-                child: Text('No schedule', style: TextStyle(fontSize: 24),
-                ),
-              ),
+            child: ListView(
+              children: [
+                ...allSchedule.map((schedule) => scheduleCard(schedule)).toList(),
+                const SizedBox(height: 16,)
+              ],
             ),
           ),
         ],
