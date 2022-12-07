4e200f798aa7b9047382a82f1e1451076872dfe2
Miran-Mirantee <killerm11805@hotmail.com>
2022-12-07 20:58:54 +0700

    Implement Edit and Add Alert

--- app/lib/pages/viewAlert.dart
+++ app/lib/pages/viewAlert.dart
@@ -36,7 +36,17 @@ class _ViewAlertState extends State<ViewAlert> {
     );
   }
 
+  // convert TimeOfDay to String
+  String formatTimeOfDay(TimeOfDay tod) {
+    String format;
+    format = '${tod.hour.toString()}:${tod.minute.toString().padLeft(2, '0')}';
+    return format;
+  }
+
   Widget alertCard(Alert alert) {
+    // convert current time from String to TimeOfDay
+    TimeOfDay time = TimeOfDay(hour:int.parse(alert.time.split(":")[0]),minute: int.parse(alert.time.split(":")[1]));
+
     return Card(
       color: Colors.blue[100],
       child: Padding(
@@ -63,10 +73,25 @@ class _ViewAlertState extends State<ViewAlert> {
                     foregroundColor: Colors.white,
                     backgroundColor: Colors.green,
                   ),
-                  onPressed: () {
-                    // go to edit page
+                  onPressed: () async {
+                    // time of new alert
+                    TimeOfDay? newTime = await showTimePicker(
+                      context: context,
+                      initialTime: time,
+                    );
+                    // if cancel return
+                    if (newTime == null) return;
+                    // if ok use new time to edit alert
+                    setState(() {
+                      // go nuts
 
-                    // change information
+                      // change time on the alert
+                      alert.time = formatTimeOfDay(newTime);
+                    });
+
+                    // change alert information
+                    //placeholderUpdateAlert();
+                    
                   },child: const Text('Edit'),
                 ),
                 TextButton(
@@ -91,12 +116,49 @@ class _ViewAlertState extends State<ViewAlert> {
 
   @override
   Widget build(BuildContext context) {
+    TimeOfDay time = TimeOfDay.now();
     return Card(
       color: Colors.red[100],
       child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Column(
           children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'List of all Alerts',
+                    style: TextStyle(
+                      fontSize: 18,
+                    ),
+                  ),
+                  TextButton(
+                    style: TextButton.styleFrom(
+                      foregroundColor: Colors.white,
+                      backgroundColor: Colors.blue,
+                    ),
+                    onPressed: () async {
+                      // time of new alert
+                      TimeOfDay? newTime = await showTimePicker(
+                        context: context,
+                        initialTime: time,
+                      );
+                      
+                      // if cancel return
+                      if (newTime == null) return;
+                      
+                      // if ok use new time to create new alert
+                      setState(() {
+                        // go nuts
+                        print(newTime);
+                      });
+                    },child: const Text('Add Alert'),
+                  ),
+                ],
+              ),
+            ),
             // display alert items
             ...allAlert.map((alert) => alertCard(alert)).toList(),
           ],
