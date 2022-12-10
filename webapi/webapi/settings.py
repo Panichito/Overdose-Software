9aec5c0fbf88ec2a3bbe8958e1b59eab952e0a33
Panichito <panithi2001big@gmail.com>
2022-12-10 10:57:36 +0700

    dj-static package

--- webapi/webapi/settings.py
+++ webapi/webapi/settings.py
@@ -27,10 +27,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'some_random_default_string')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG', False)
-DEBUG = False
+DEBUG = os.environ.get('DEBUG', False)
 
-ALLOWED_HOSTS = ['weatherreporto.pythonanywhere.com', '*']
+ALLOWED_HOSTS = ['weatherreporto.pythonanywhere.com']
 
 
 # Application definition
