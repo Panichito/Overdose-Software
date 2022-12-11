312d214771043817fdf0b2216cdff45a66b3b8be
pupshi <72983675+pupshi@users.noreply.github.com>
2022-12-11 21:08:54 +0700

    Design change

--- app/lib/pages/recordDetails.dart
+++ app/lib/pages/recordDetails.dart
@@ -24,7 +24,10 @@ class RecordInformation {
       this.patientName,
       this.medicineName,
       this.disease,
-      this.amount, this.startDate, this.endDate, this.note);
+      this.amount,
+      this.startDate,
+      this.endDate,
+      this.note);
 }
 
 class RecordDetailsPage extends StatefulWidget {
@@ -44,30 +47,60 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
   void initState() {
     // TODO: implement initState
     super.initState();
-    _v1=widget.v1;
-    _v2=widget.v2;
-    _v3=widget.v3;
-    _v4=widget.v4;
-    _v5=widget.v5;
-    _v6=widget.v6;
-    _v7=widget.v7;
-    _v8=widget.v8;
-    _v9=widget.v9;
-    _v10=widget.v10;
-    record = RecordInformation(_v1, _v2, _v3, _v4, _v5, _v6, _v7, _v8, _v9, _v10);
+    _v1 = widget.v1;
+    _v2 = widget.v2;
+    _v3 = widget.v3;
+    _v4 = widget.v4;
+    _v5 = widget.v5;
+    _v6 = widget.v6;
+    _v7 = widget.v7;
+    _v8 = widget.v8;
+    _v9 = widget.v9;
+    _v10 = widget.v10;
+    record =
+        RecordInformation(_v1, _v2, _v3, _v4, _v5, _v6, _v7, _v8, _v9, _v10);
   }
-
 
   // text in record information
   // more convenient when styling
   Widget _recordInfoText(text) {
     return Text(
       text,
-      style: const TextStyle(fontSize: 16),
+      style: const TextStyle(fontSize: 10, fontStyle: FontStyle.italic),
     );
   }
 
   @override
+  Widget styleForRecordInfoCard(IconData icon, text1, text2) {
+    return Row(
+      children: [
+        Padding(
+          padding: EdgeInsets.only(right: 5),
+          child: Icon(
+            icon,
+            size: 35,
+            color: Colors.indigo[400],
+          ),
+        ),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              text1,
+              style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.grey[800],
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 3),
+            Text(text2,
+                style: TextStyle(fontSize: 12.0, color: Colors.grey[800]))
+          ],
+        )
+      ],
+    );
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -82,51 +115,71 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
             child: Column(
               children: [
                 Card(
-                  color: Colors.red[100],
+                  color: Colors.white,
+                  elevation: 8,
                   child: Padding(
                     padding: const EdgeInsets.all(12.0),
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        _recordInfoText('Record ID: R${record!.recordId}'),
+                        Text('Record Information',
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.indigo[400],
+                                fontFamily: 'QuickSand')),
+                        SizedBox(height: 10),
+                        styleForRecordInfoCard(Icons.note_alt, 'Record ID',
+                            'R${record!.recordId}'),
                         const SizedBox(
-                          height: 8,
+                          height: 12,
                         ),
-                        _recordInfoText('Patient: ${record!.patientName}'),
+                        styleForRecordInfoCard(
+                            Icons.person, 'Patient', '${record!.patientName}'),
                         const SizedBox(
-                          height: 8,
+                          height: 12,
                         ),
-                        _recordInfoText('Medicine: ${record!.medicineName}'),
+                        styleForRecordInfoCard(
+                            Icons.sick, 'Disease', '${record!.disease}'),
                         const SizedBox(
-                          height: 8,
+                          height: 12,
                         ),
-                        _recordInfoText('Disease: ${record!.disease}'),
+                        styleForRecordInfoCard(Icons.medication, 'Medicine',
+                            '${record!.medicineName}'),
                         const SizedBox(
-                          height: 8,
+                          height: 5,
                         ),
-                        _recordInfoText('Amount: ${record!.amount.toString()}'),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        _recordInfoText('Start Date: ${record!.startDate}'),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        _recordInfoText('End Date: ${record!.endDate}'),
-                        const SizedBox(
-                          height: 8,
-                        ),
-                        _recordInfoText('Note: ${record!.note}'),
-                        const SizedBox(
-                          height: 16,
-                        ),
+                        Padding(
+                            padding: EdgeInsets.only(left: 50),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                _recordInfoText(
+                                    'Amount: ${record!.amount.toString()}'),
+                                const SizedBox(
+                                  height: 6,
+                                ),
+                                _recordInfoText(
+                                    'Start Date: ${record!.startDate}'),
+                                const SizedBox(
+                                  height: 6,
+                                ),
+                                _recordInfoText('End Date: ${record!.endDate}'),
+                                const SizedBox(
+                                  height: 12,
+                                ),
+                              ],
+                            )),
+                        styleForRecordInfoCard(
+                            Icons.edit_note, 'Note', '${record!.note}'),
+                        SizedBox(height: 10),
                         Row(
                           mainAxisAlignment: MainAxisAlignment.center,
                           children: [
                             TextButton(
                               style: TextButton.styleFrom(
                                 foregroundColor: Colors.white,
-                                backgroundColor: Colors.green,
+                                backgroundColor: Colors.indigo[400],
                               ),
                               onPressed: () {
                                 // go to edit page
@@ -143,18 +196,19 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
                                             record!.endDate,
                                             record!.amount,
                                             record!.note))).then((value) {
-                                              setState(() {
-                                                Map<String, dynamic> map = jsonDecode(value);
-                                                record!.medicineId = map['medicine'];
-                                                refreshMedName(map['medicine']);
-                                                record!.disease = map['Record_disease'];
-                                                record!.amount = map['Record_amount'];
-                                                record!.startDate = map['Record_start'];
-                                                record!.endDate = map['Record_end'];
-                                                record!.note = map['Record_info'];
-                                                //refreshRecord(record!.recordId);
-                                              });
-                                            });
+                                  setState(() {
+                                    Map<String, dynamic> map =
+                                        jsonDecode(value);
+                                    record!.medicineId = map['medicine'];
+                                    refreshMedName(map['medicine']);
+                                    record!.disease = map['Record_disease'];
+                                    record!.amount = map['Record_amount'];
+                                    record!.startDate = map['Record_start'];
+                                    record!.endDate = map['Record_end'];
+                                    record!.note = map['Record_info'];
+                                    //refreshRecord(record!.recordId);
+                                  });
+                                });
                                 // change information
                               },
                               child: const Text('Edit'),
@@ -165,49 +219,7 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
                             TextButton(
                               style: TextButton.styleFrom(
                                 foregroundColor: Colors.white,
-                                backgroundColor: Colors.red,
-                              ),
-                              onPressed: () {
-                                // ask for confirmation
-                                showDialog(
-                                    context: context,
-                                    builder: (context) => AlertDialog(
-                                            title: const Text(
-                                                '🗑️ Delete Record!'),
-                                            content: const Text(
-                                                'All of the alert of this record will be deleted. Do you wish to proceed?'),
-                                            actions: [
-                                              TextButton(
-                                                // cancel
-                                                onPressed: () {
-                                                  Navigator.pop(context);
-                                                },
-                                                child: const Text('Cancel'),
-                                              ),
-                                              TextButton(
-                                                onPressed: () async {
-                                                  // delete information
-                                                  await deleteRecord(record!.recordId);
-                                                  int count = 0;
-                                                  // pop to RecordPage
-                                                  Navigator.popUntil(context,
-                                                      (route) {
-                                                    return count++ == 2;
-                                                  });
-                                                },
-                                                child: const Text('OK'),
-                                              ),
-                                            ]));
-                              },
-                              child: const Text('Delete'),
-                            ),
-                            const SizedBox(
-                              width: 8,
-                            ),
-                            TextButton(
-                              style: TextButton.styleFrom(
-                                foregroundColor: Colors.white,
-                                backgroundColor: Colors.blue,
+                                backgroundColor: Colors.indigo[400],
                               ),
                               onPressed: () {
                                 // ask for confirmation
@@ -229,10 +241,15 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
                                               TextButton(
                                                 onPressed: () async {
                                                   // mark the record complete
-                                                  await markRecordDone(record!.recordId, record!.patientId, record!.medicineId, record!.disease);
+                                                  await markRecordDone(
+                                                      record!.recordId,
+                                                      record!.patientId,
+                                                      record!.medicineId,
+                                                      record!.disease);
                                                   int count = 0;
                                                   // pop to RecordPage
-                                                  Navigator.popUntil(context, (route) {
+                                                  Navigator.popUntil(context,
+                                                      (route) {
                                                     return count++ == 2;
                                                   });
                                                 },
@@ -241,7 +258,50 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
                                             ]));
                               },
                               child: const Text('Mark as Complete'),
-                            )
+                            ),
+                            const SizedBox(
+                              width: 8,
+                            ),
+                            TextButton(
+                              style: TextButton.styleFrom(
+                                foregroundColor: Colors.white,
+                                backgroundColor: Colors.red[400],
+                              ),
+                              onPressed: () {
+                                // ask for confirmation
+                                showDialog(
+                                    context: context,
+                                    builder: (context) => AlertDialog(
+                                            title: const Text(
+                                                '🗑️ Delete Record!'),
+                                            content: const Text(
+                                                'All of the alert of this record will be deleted. Do you wish to proceed?'),
+                                            actions: [
+                                              TextButton(
+                                                // cancel
+                                                onPressed: () {
+                                                  Navigator.pop(context);
+                                                },
+                                                child: const Text('Cancel'),
+                                              ),
+                                              TextButton(
+                                                onPressed: () async {
+                                                  // delete information
+                                                  await deleteRecord(
+                                                      record!.recordId);
+                                                  int count = 0;
+                                                  // pop to RecordPage
+                                                  Navigator.popUntil(context,
+                                                      (route) {
+                                                    return count++ == 2;
+                                                  });
+                                                },
+                                                child: const Text('OK'),
+                                              ),
+                                            ]));
+                              },
+                              child: const Text('Delete'),
+                            ),
                           ],
                         )
                       ],
@@ -259,7 +319,8 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
   }
 
   Future<void> refreshMedName(int mid) async {
-    var url = Uri.https('weatherreporto.pythonanywhere.com', '/api/medicine-info/$mid');
+    var url = Uri.https(
+        'weatherreporto.pythonanywhere.com', '/api/medicine-info/$mid');
     var response = await http.get(url);
     var result = utf8.decode(response.bodyBytes);
     setState(() {
@@ -269,15 +330,17 @@ class _RecordDetailsPageState extends State<RecordDetailsPage> {
   }
 
   Future<void> deleteRecord(int rid) async {
-    var url = Uri.https('weatherreporto.pythonanywhere.com', '/api/delete-record/$rid');
-    Map<String, String> header={"Content-type":"application/json"};
-    var response=await http.delete(url, headers: header);
+    var url = Uri.https(
+        'weatherreporto.pythonanywhere.com', '/api/delete-record/$rid');
+    Map<String, String> header = {"Content-type": "application/json"};
+    var response = await http.delete(url, headers: header);
     print(response.body);
   }
 
   Future<void> markRecordDone(int rid, int pid, int mid, String disease) async {
-    var url = Uri.https('weatherreporto.pythonanywhere.com', '/api/update-record/$rid');
-    Map<String, String> header={"Content-type":"application/json"};
+    var url = Uri.https(
+        'weatherreporto.pythonanywhere.com', '/api/update-record/$rid');
+    Map<String, String> header = {"Content-type": "application/json"};
     // required 4 fields
     String v1 = '"patient":$pid';
     String v2 = '"medicine":$mid';
